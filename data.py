f1531124f7b0ec2d9e973144b046736d23eb622e
AmrMualla <75446810+AmrMualla@users.noreply.github.com>
2021-07-04 01:02:04 +0000

    Initial commit

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+  {"text": "Greenland is covered with grass and Iceland covered with ice.", "answer": "False"},
+  {"text": "Vatican City is a country", "answer": "True"},
+  {"text": "Alaska is the largest state in the United States.", "answer": "True"},
+  {"text": "Ottawa is the capital of Canada.", "answer": "True"},
+  {"text": "There are no deserts in Europe", "answer": "True"},
+  {"text": "Toronto is the capital city of the North American country of Canada.", "answer": "False"},
+  {"text": "Tokyo is the capital of Japan.", "answer": "True"},
+  {"text": "Hungary is the only country in the world beginning with H.", "answer": "False"},
+  {"text": "New Haven is the capital city of the state of Connecticut in the United States.", "answer": "False"},
+  {"text": "Rhode Island is actually located on the US mainland, despite its name.", "answer": "True"},
+  {"text": "Baghdad is the capital of Syria", "answer": "False"},
+  {"text": "The city of Dubai is in Europe.", "answer": "False"}
+]
